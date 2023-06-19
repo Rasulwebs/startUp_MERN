ec7be106830cb91f5f931e53f8fe4634f8d67483
najmiddinnazarov <najmiddinnazarov0405@gmail.com>
2023-06-19 22:08:46 +0500

    Add forgot password page

--- src/auth/Update/ConfirmNewPassword.jsx
+++ src/auth/Update/ConfirmNewPassword.jsx
@@ -1,19 +1,72 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import Layout from "../../components/Layout/Layout";
-import "./forgotPasswordStyle.scss"
+import "./forgotPasswordStyle.scss";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
+const BASE_URL = "https://startupwepapp.onrender.com";
+
 const ConfirmNewPassword = () => {
-  const [verifCode, setverifCode] = useState("");
-  const [newPass, setNewPass] = useState("");
+  const navigate = useNavigate();
+  const [success, setSuccess] = useState();
+  const [value, setValue] = useState({
+    email: "",
+    password: "",
+    confirmationCode: "",
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post(`${BASE_URL}/api/users/forget`, {
+        email: sessionStorage.getItem("email"),
+        password: value.password,
+        confirmationCode:
+          value.confirmationCode === sessionStorage.getItem("confirmationCode")
+            ? setSuccess(true)
+            : alert("The verification password is not correct!"),
+      })
+      .then((res) => {
+        return res;
+      })
+      .catch(({ name, message }) => console.log(name, message));
+  };
+
   return (
     <>
+      {success && navigate("/")}
       <Layout>
         <div className="container">
           <div className="formWrapp">
-          <h2 className="text-center text-white mb-4">Verify and New Password</h2>
-
-            <input type="text" required className="form-control bg-transparent" placeholder="Enter verification code" />
-            <input type="text" required className="form-control bg-transparent" placeholder="Enter New Password " />
-            <button className="btn py-3 btn-success w-100 mt-3">SEND</button>
+            <h2 className="text-center text-white mb-4">
+              Verify and New Password
+            </h2>
+            <form onSubmit={handleSubmit}>
+              <input
+                type="text"
+                required
+                className="form-control bg-transparent"
+                placeholder="Enter verification code"
+                onChange={(e) =>
+                  setValue({ ...value, confirmationCode: e.target.value })
+                }
+                name="text"
+                minLength="6"
+              />
+              <input
+                type="password"
+                required
+                className="form-control bg-transparent"
+                placeholder="Enter New Password "
+                onChange={(e) =>
+                  setValue({ ...value, password: e.target.value })
+                }
+                name="password"
+                minLength="6"
+                maxLength="12"
+              />
+              <button className="btn py-3 btn-success w-100 mt-3">SEND</button>
+            </form>
           </div>
         </div>
       </Layout>
--- src/auth/Update/ForgotPassword.jsx
+++ src/auth/Update/ForgotPassword.jsx
@@ -1,24 +1,51 @@
-import React from "react";
+// import React from "react";
 import "./forgotPasswordStyle.scss";
 import Layout from "../../components/Layout/Layout";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import axios from "axios";
+
+const BASE_URL = "https://startupwepapp.onrender.com";
 
 const ForgotPassword = () => {
-  const navigate =useNavigate()
-  function handlersubmit(e) {
+  const navigate = useNavigate();
+  const [success, setSuccess] = useState();
+  const [value, setValue] = useState({
+    email: "",
+  });
+
+  const handleSubmit = (e) => {
     e.preventDefault();
-    navigate("/confitrm-new-password")
-  }
+    axios
+      .post(`${BASE_URL}/api/users/forget`, {
+        email: value.email,
+      })
+      .then((res) => {
+        sessionStorage.setItem("confirmationCode", res.data.confirmationCode);
+        sessionStorage.setItem("email", value.email);
+        setSuccess(true);
+      })
+      .catch(({ name, message }) => console.log(name, message));
+  };
+
   return (
     <>
+      {success && navigate("/confitrm-new-password")}
       <Layout title={"Forgot password page"}>
         <div className="update-container">
-          <form className="update" onSubmit={handlersubmit}>
+          <form className="update" onSubmit={handleSubmit}>
             <h2 className="text-center text-white">UPDATE</h2>
-            <input type="email" placeholder="Enter you email" />
-
-           
-            <button type={"submit"} className="btn btn-success mt-3 py-3" >SEND</button>
+            <input
+              type="email"
+              placeholder="Enter your email"
+              name="email"
+              className="form-control bg-transparent"
+              onChange={(e) => setValue({ ...value, email: e.target.value })}
+              required
+            />
+            <button type={"submit"} className="btn btn-success mt-3 py-3">
+              SEND
+            </button>
           </form>
         </div>
       </Layout>
